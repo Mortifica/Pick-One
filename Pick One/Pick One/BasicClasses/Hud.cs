5ae2bdb5987e50b93fc8738e5cd3199eafe4fd36
Josh <josh.hanke@gmail.com>
2016-04-17 14:57:07 -0500

    added a hud

---
+++ Pick One/Pick One/BasicClasses/Hud.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pick_One.BasicClasses
+{
+    public class Hud
+    {
+        private SpriteFont font;
+        private MainGameLoop game;
+
+        public Hud(MainGameLoop gameLoop, SpriteFont spriteFont)
+        {
+            game = gameLoop;
+            font = spriteFont;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(font,"Test Text",game.Camera.Focus.Location - new Vector2(200,200), Color.White);
+        }
+    }
+}
